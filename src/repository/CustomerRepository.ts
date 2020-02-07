0be269aab6cfa9ef4da7c24bab3973dafac457e9
mohammadkarimian <m.karimian1993@gmail.com>
2020-02-07 19:36:37 +0330

    create customer repository

--- src/repository/CustomerRepository.ts
+++ src/repository/CustomerRepository.ts
@@ -1,10 +1,10 @@
 import { Customer } from "../entity/Customer"
 import { Repository, Connection } from "typeorm"
 
-export class CustomerRepository{
+export class CustomerRepository {
     private repository: Repository<Customer>
 
-    constructor(connection: Connection) { 
+    constructor(connection: Connection) {
         this.repository = connection.getRepository(Customer)
     }
 }
