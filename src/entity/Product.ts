b98bf9fc7ba188948fa8ec593227d7da7aa2bcd3
mohammadkarimian <m.karimian1993@gmail.com>
2020-02-07 15:06:42 +0330

    create Product entity

---
+++ src/entity/Product.ts
@@ -0,0 +1,43 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from "typeorm";
+import { Customer } from "./Customer";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    private id: number
+
+    @Column({type: "string",length: 32})
+    private name: string
+
+    @OneToOne(type => Customer)
+    @JoinColumn()
+    private customer: Customer
+
+    getId(): number {
+        return this.id
+    }
+
+    setId(id: number): Product {
+        this.id = id
+        return this
+    }
+
+    getName(): string {
+        return this.name
+    }
+
+    setName(name: string): Product {
+        this.name = name
+        return this
+    }
+
+    getCustomer(): Customer {
+        return this.customer
+    }
+
+    setCustomer(customer: Customer): Product {
+        this.customer = customer
+        return this
+    }
+}
